b404d676c84b02f551ece5f0fce712a6f51ca86e
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-08-29 21:55:43 +0100

    styled-components is added to project package (This library is quite awesome!)

--- src/App.js
+++ src/App.js
@@ -5,14 +5,42 @@ import MyButton from "./MyButton";
 import {styles} from "./style";
 import {Box} from "./Box";
 import Shadow from "./Shadow";
+import styled from "styled-components/native";
+import {css} from "styled-components";
+
+const Container = styled.View`
+    flex: 1;
+    background-color: #e21;
+    align-items: center;
+    justify-content: center;
+`
+
+const cssText = css`
+  font-size: 20px;
+  font-weight: 600;
+`
+
+// 이렇게 CSS를 불러올수도 있고
+const StyledText = styled.Text`
+  ${cssText}
+  color: blue;
+`
+
+// 이렇게 각 스타일을 상속받을 수도 있다.
+const ErrorText = styled(StyledText)`
+  color: aquamarine;
+`
+
+const StyledButton = styled.Button``
 
 export default function App() {
     return (
-        <View style={styles.container}>
+        <Container>
             <StatusBar style="auto"/>
-            <Shadow></Shadow>
-
-        </View>
+            <StyledText>Styled Components</StyledText>
+            <StyledButton title="styled button" onPress={() => alert('HELLO!')}/>
+            <ErrorText>This is Error Text</ErrorText>
+        </Container>
     );
 }
 
---
+++ src/Input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from "styled-components/native";
+import {StyleSheet} from "react-native-web";
+
+const StyledInput = styled.TextInput`
+  padding: 20px;
+  font-size: 20px;
+  border: 1px solid #111111;
+`
+
+const Input = () => {
+    return <StyleSheet />
+}
+
+export default Input
